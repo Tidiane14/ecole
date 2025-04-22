770157a75c51a9ed97bbfdde46dd0dfbad484486
Rougss <rouguiatouba640@gmail.com>
2025-04-22 15:19:57 +0000

    its caissiere

--- app/Models/utilisateur.php
+++ app/Models/utilisateur.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\admin;
 
 class utilisateur extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'id_user',
+        'nom',
+        'prenom',
+        'email',
+        'password',
+        'role',
+    ];
+    public function caissiere()
+    {
+        return $this->hasOne(caissiere::class, 'id_user', 'id_user');
+    }
+    
+ 
+    
+   
 }
