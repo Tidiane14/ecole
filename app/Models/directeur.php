01458af518d452867a1e806fe9c40d39c2a28000
LOMBAYE <lombayedev@gmail.com>
2025-04-22 15:08:24 +0000

    youssef

---
+++ app/Models/directeur.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class directeur extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'nom',
+        'prenom',
+        'email',
+        'mot_de_passe',
+        'id_role'
+    ];
+    
+    //
+}
